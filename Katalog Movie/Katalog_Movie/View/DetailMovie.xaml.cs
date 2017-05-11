8ef9c0e490b504d93095f0a9ad39ffaee020b0de
Ramdhan Rizki <ramdhanrizki11@email.unikom.ac.id>
2017-05-11 09:15:36 +0700

    Initial Commit

---
+++ Katalog Movie/Katalog_Movie/View/DetailMovie.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Katalog_Movie.Model;
+
+namespace Katalog_Movie.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailMovie : ContentPage
+    {
+        public DetailMovie()
+        {
+            InitializeComponent();
+            //this.BindingContext = item;
+            btnBack.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopAsync(true);
+            };
+        }
+    }
+}
---
+++ Katalog Movie/Katalog_Movie/View/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Katalog_Movie.ViewModel;
+using Katalog_Movie.Model;
+using Katalog_Movie.View;
+
+namespace Katalog_Movie.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            VMMovieList vmmovie = new VMMovieList();
+            listView.BindingContext = vmmovie;
+
+            listView.ItemTapped += async (sender, e) => {
+                //if (e.SelectedItem == null) return; 
+                Movie movie = (Movie)e.Item;
+                await Task.Yield();
+                //await Navigation.PushAsync(new DetailMovie(movie));
+
+                var detailMovie= new DetailMovie();
+                detailMovie.BindingContext = movie;
+                await Navigation.PushAsync(detailMovie);
+                ((ListView)sender).SelectedItem = null;
+            };
+        }
+    }
+}
